917c47821ac338a6f9ceb52d744b6f39a69040f1
kaitlynsandor <kaitlyn.sandor@gmail.com>
2022-04-14 14:02:27 -0400

    added pictures for leaflet

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
