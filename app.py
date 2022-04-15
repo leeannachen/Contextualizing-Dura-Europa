e35ff0863619308639b5f2a496d73df0bc08e93c
kaitlynsandor <kaitlyn.sandor@gmail.com>
2022-04-15 11:40:31 -0400

    changed db name

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = None#SQL("sqlite:///finance.db")
+db = None#SQL("sqlite:///sample.db")
 
 # # Make sure API key is set
 # if not os.environ.get("API_KEY"):
