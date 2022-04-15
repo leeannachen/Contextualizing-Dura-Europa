08241a680478e48ae13dbf89176c8a4e678f2386
kaitlynsandor <kaitlyn.sandor@gmail.com>
2022-04-15 11:13:17 -0400

    commiting some comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-#from cs50 import SQL
+import sqlite3
+from sqlite3 import Error
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -39,75 +40,26 @@ def after_request(response):
 
 
 @app.route("/")
-@login_required
 def index():
     """Show portfolio of stocks"""
     return apology("TODO")
 
 
 @app.route("/buy", methods=["GET", "POST"])
-@login_required
 def buy():
     """Buy shares of stock"""
     return apology("TODO")
 
 
 @app.route("/history")
-@login_required
 def history():
     """Show history of transactions"""
     return apology("TODO")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
 
 
 @app.route("/quote", methods=["GET", "POST"])
-@login_required
 def quote():
     """Get stock quote."""
     return apology("TODO")
@@ -120,7 +72,6 @@ def register():
 
 
 @app.route("/sell", methods=["GET", "POST"])
-@login_required
 def sell():
     """Sell shares of stock"""
     return apology("TODO")
